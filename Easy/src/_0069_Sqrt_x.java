f259d6e8cf9b12cf50f7778f59e00f5d1042aa49
jinzhu2017 <jinzhu2017@gmail.com>
2020-09-24 17:37:39 +0200

    69. Sqrt(x)

---
+++ Easy/src/_0069_Sqrt_x.java
@@ -0,0 +1,13 @@
+public class _0069_Sqrt_x {
+    public static int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+
+    public static void main(String[] args) {
+        int x1 = 4;
+        System.out.println(mySqrt(x1));
+
+        int x2 = 8;
+        System.out.println(mySqrt(x2));
+    }
+}
