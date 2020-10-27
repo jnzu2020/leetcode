fdae2bb34f3e03563433abd91350b033a8962064
jinzhu2017 <jinzhu2017@gmail.com>
2020-10-27 10:32:12 +0100

    206. Reverse Linked List

---
+++ Easy/src/_0206_Reverse_Linked_List.java
@@ -0,0 +1,26 @@
+public class _0206_Reverse_Linked_List {
+    public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode pre = null;
+        while(current != null) {
+            ListNode next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        System.out.println(reverseList(listNode1));
+    }
+}
