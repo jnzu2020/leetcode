86fb928f1385d594a7665e2db28ea66dc17a2562
jinzhu2017 <jinzhu2017@gmail.com>
2020-11-15 15:03:32 +0100

    303. Range Sum Query - Immutable

---
+++ Easy/src/_0303_Range_Sum_Query_Immutable.java
@@ -0,0 +1,25 @@
+public class _0303_Range_Sum_Query_Immutable {
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        NumArray numArray = new NumArray(nums);
+        System.out.println(numArray.sumRange(0, 2));
+        System.out.println(numArray.sumRange(2, 5));
+        System.out.println(numArray.sumRange(0, 5));
+    }
+}
+
+class NumArray {
+    final private int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        for (int index = i ; index <= j ; index++ ) {
+            sum += this.nums[index];
+        }
+        return sum;
+    }
+}
