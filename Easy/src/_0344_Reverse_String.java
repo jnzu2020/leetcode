d02a36c3c3011c8be7f6a4ff5f6e1dced137735f
jinzhu2017 <jinzhu2017@gmail.com>
2020-11-17 09:58:11 +0100

    344. Reverse String

---
+++ Easy/src/_0344_Reverse_String.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class _0344_Reverse_String {
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s1 = {'h','e','l','l','o'};
+        reverseString(s1);
+        System.out.println(Arrays.toString(s1));
+
+        char[] s2 = {'H','a','n','n','a','h'};
+        reverseString(s2);
+        System.out.println(Arrays.toString(s2));
+    }
+}
